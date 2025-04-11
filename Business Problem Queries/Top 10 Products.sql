c7e247f9f05f6acd31297d271b7bfe286511cd18
Ankush Shukla <ankushshukla0699@gmail.com>
2025-04-12 01:24:24 +0530

    renamed file

--- Business Problem Queries/Top 10 Products.sql
+++
@@ -1,9 +0,0 @@
--- Q1: find top 10 highest revenue generating products.
-
-SELECT 
-    product_id, SUM(sale_price) AS revenue
-FROM
-    df_orders
-GROUP BY product_id
-ORDER BY revenue DESC
-LIMIT 10;
